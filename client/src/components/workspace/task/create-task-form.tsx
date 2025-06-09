2fd341926b0241d35b31ddaa88e907231b4d252f
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2025-06-10 01:43:25 +0530

    completed with recent tasks, projects & joins

--- client/src/components/workspace/task/create-task-form.tsx
+++ client/src/components/workspace/task/create-task-form.tsx
@@ -160,6 +160,11 @@ export default function CreateTaskForm(props: {
         queryClient.invalidateQueries({
           queryKey: ["project-analytics", projectId],
         });
+
+        queryClient.invalidateQueries({
+          queryKey: ["all-tasks", workspaceId],
+        });
+
         toast({
           title: "Success",
           description: "Task created Successfully",
