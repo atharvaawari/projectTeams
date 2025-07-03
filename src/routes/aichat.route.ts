3bb8301c8aded436314b6e40eb2eaaec07a7ac96
Atharva Awari <140247890+atharvaawari@users.noreply.github.com>
2025-07-04 01:44:02 +0530

    renamed

--- src/routes/aichat.route.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getAiResponseController } from "../controllers/ai.controller";
-
-const chatRouter = Router();
-
-chatRouter.post('/query', getAiResponseController);
-
-export default chatRouter;
